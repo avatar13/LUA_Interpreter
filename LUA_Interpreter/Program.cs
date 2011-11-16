0d623600380d550db229c581ab17df0321e20443
unknown <C:\The Bat!>
2011-11-16 20:12:25 +0300

    new

---
+++ LUA_Interpreter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LUA_Interpreter;
+
+namespace LUA_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser();
+
+            System.IO.TextReader reader;
+            if (args.Length > 0)
+                reader = new System.IO.StreamReader(args[0]);
+            else
+                reader = System.Console.In;
+            
+            parser.Scanner = new Lexer(reader);
+                        
+            parser.Parse();
+        }
+    }
+}
